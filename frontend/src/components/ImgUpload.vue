afcc80c5f129ea091c60762592c7b1d2a46bd77f
cluchen0000 <luchen81@gmail.com>
2026-05-20 16:32:16 +0800

    Clean frontend directory - keep only .keep files

--- frontend/src/components/ImgUpload.vue
+++
@@ -1,2 +0,0 @@
-<!-- 预留：可抽取的上传组件，当前在 SingleDetect.vue 中内联实现 -->
-<template><div></div></template>
--- frontend/src/components/ResultShow.vue
+++
@@ -1,2 +0,0 @@
-<!-- 预留：可抽取的结果展示组件，当前在 SingleDetect.vue 中内联实现 -->
-<template><div></div></template>
